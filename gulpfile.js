b71ef5607d6c651b164f2dc9a26f95c4aa5722c6
Fernando MAdS <fermads@gmail.com>
2015-03-16 02:07:43 -0300

    adding dev files

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var package = require('./package.json');
+
+gulp.task('default', function() {
+  gulp.src('src/*.js')
+    .pipe(uglify())
+    .pipe(rename(package.name +'-'+ package.version +'.min.js'))
+    .pipe(gulp.dest('dist/'));
+  gulp.src('src/*.css')
+    .pipe(minify({keepBreaks:true}))
+    .pipe(rename(package.name +'-'+ package.version +'.min.css'))
+    .pipe(gulp.dest('dist/'));
+});
