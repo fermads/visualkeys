fc1cf6ea1d94639876afaa971e0775d17ac66fc0
Fernando MAdS <fermads@gmail.com>
2015-03-17 02:22:42 -0300

    example page

--- web/index.js
+++ web/index.js
@@ -1,19 +1,28 @@
-function insertAtCaret(areaId,text) { var txtarea = document.getElementById(areaId); var scrollPos = txtarea.scrollTop; var strPos = 0; var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false ) ); if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); strPos = range.text.length; } else if (br == "ff") strPos = txtarea.selectionStart; var front = (txtarea.value).substring(0,strPos); var back = (txtarea.value).substring(strPos,txtarea.value.length); txtarea.value=front+text+back; strPos = strPos + text.length; if (br == "ie") { txtarea.focus(); var range = document.selection.createRange(); range.moveStart ('character', -txtarea.value.length); range.moveStart ('character', strPos); range.moveEnd ('character', 0); range.select(); } else if (br == "ff") { txtarea.selectionStart = strPos; txtarea.selectionEnd = strPos; txtarea.focus(); } txtarea.scrollTop = scrollPos; }
+$.visualkeys({
+  '86' : {
+    text : '<i>v</i><b>View options</b> configuration for this demo',
+    exec : function(){location.href = 'index.js';}
+  },
+  '65' : {
+    text : '<i>a</i><b>This will be hidden because "hide" returns true</b> ',
+    exec : function(){},
+    hide : function() {return $('h1').length === 1;}
+  },
+  '83' : {
+    text : '<i>s</i><b>Save</b> your current work',
+    exec : function(){}
+  },
+  '39' : {
+    text : '<i>&rarr;</i>Align all text to the <b>right</b>',
+    exec : function(){$('body').css('text-align','right');}
 
-Main = {
-	
-	init : function(){
-		
-		Main.email();
-	},
-	
-	email : function() {
-		var email = 'fermads'+'@'+'gmail.com'
-		$('#email').html(email);
-		$('#email').attr('href', email); 
-		
-	}
-	
-}
-
-$(Main.init);
+  },
+  '66' : {
+    text : '<i>b</i>Change <b>background to red</b>',
+    exec : function(){$('body').css('background','red');}
+  },
+  '71' : {
+    text : '<i>g</i><b>go to Disney</b> Land',
+    exec : function(){ location.href='http://disney.com/';}
+  }
+});
