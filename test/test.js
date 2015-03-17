a02bfd6d896d816056fe4df378ea3fc71dce9753
Fernando MAdS <fermads@gmail.com>
2015-03-17 02:22:16 -0300

    preparation for new QUnit tests

--- test/test.js
+++ test/test.js
@@ -1,53 +1,14 @@
-module('Core');
 
-vk = $('#vk');
+QUnit.test('visualkeys init', function( assert ) {
+  $.visualkeys({
+    66 : {
+      text : '<i>b</i>Change <b>background to red</b>',
+      exec : function(){$('body').css('background','red')}
+    }
+  });
 
-test('', function(){
-	expect(4);
-	ok( $.visualkeys, '$.visualkeys object' )
-	$.visualkeys({
-		66 : {
-			text : '<i>b</i><b>bbb</b>',
-			exec : function(k){
-				start();
-				ok($('#visualkeys i').html() == 'b' 
-					&& $('#visualkeys ul').length == 1
-					&& $('#visualkeys b').length == 1, 'initialization')
-				ok(k == 66, '$.visualkeys([key])')
-				$.visualkeys('remove');
-				ok($('#visualkeys').length == 0, '$.visualkeys(\'remove\')')
-				 
-			}
-		}
-	});
-	
-	stop();
-	$.visualkeys(66);
+  assert.ok($('#visualkeys').length === 1);
 
-})
-
-module('Parameters');
-
-test('', function(){
-
-	$.visualkeys({
-		65 : {
-			text : '<i>a</i><b>description text</b>',
-			exec : function(k){
-				start();
-				vk.html(k);
-				ok( $('#visualkeys i').html() == 'a', 'parameter overwrite' )
-				equals( $('#visualkeys i').html(), 'a', 'key' )
-				equals( $('#visualkeys b').html(), 'description text', 'text' )
-				ok( $('#visualkeys i').length, 'hide' )
-				equals( vk.html(), '65', 'exec' )
-			},
-			hide : false
-		}
-	})
-
-	expect(5)
-	stop();
-	$.visualkeys(65);
 
 });
+
